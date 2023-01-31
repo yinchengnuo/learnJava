bf1ba7ab1c45a6dc2082ca917a5588f1aafc401b
尹成诺 <yinchengnuo@qq.com>
2023-01-31 12:24:39 +0800

    Hello World

---
+++ src/HelloWorld/HelloWorld.java
@@ -0,0 +1,12 @@
+package HelloWorld;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public static void say() {
+        System.out.println("SAY: Hello World!");
+    }
+}
